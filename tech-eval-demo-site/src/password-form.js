02806896a261e4b41eab7198159975482b2b4087
KyleIsaak <55521911+KyleIsaak@users.noreply.github.com>
2021-11-05 22:32:02 -0700

    testing function calls

--- tech-eval-demo-site/src/password-form.js
+++ tech-eval-demo-site/src/password-form.js
@@ -1,11 +1,24 @@
+import { observable } from "../node_modules/aurelia-binding/dist/aurelia-binding";
+
 export class PasswordForm {
   constructor() {
     this.password = '';
-    this.strength = "0";
+    this.strength = 0;
     this.hasCaps = false;
     this.hasLower = false;
     this.hasSymbols = false;
     this.passLength = false;
   }
 }
-  
+
+
+document.getElementById('inputBar').getElementById('passInput').oninput = function() {updateRequirements()};
+
+function updateRequirements(){
+  //if this.password
+  alert("The value of the input field was changed.");
+  //document.getElementById("caps").innerText.fontcolor = "green"
+  //this.strength = this.strength + 1
+
+  //document.getElementById("password-strength-meter").value = "2";
+}
