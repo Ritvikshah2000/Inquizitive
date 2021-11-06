ae9464fd2019c57d3bdf82fce9f68e981b2976d8
KyleIsaak <55521911+KyleIsaak@users.noreply.github.com>
2021-11-05 23:17:23 -0700

    All functions working correctly. TODO: Make it look pretty

--- tech-eval-demo-site/src/password-form.js
+++ tech-eval-demo-site/src/password-form.js
@@ -1,24 +1,90 @@
-import { observable } from "../node_modules/aurelia-binding/dist/aurelia-binding";
 
 export class PasswordForm {
   constructor() {
     this.password = '';
     this.strength = 0;
-    this.hasCaps = false;
-    this.hasLower = false;
-    this.hasSymbols = false;
-    this.passLength = false;
   }
+
+  updateRequirements(){
+    this.strength = 0;
+
+    if (hasUpper(this.password)){
+      document.getElementById("caps").style.color = "green"
+      this.strength++
+    }
+    else{
+      document.getElementById("caps").style.color = "red"
+    }
+
+    if (hasLower(this.password)){
+      document.getElementById("lower").style.color = "green"
+      this.strength++
+    }
+    else{
+      document.getElementById("lower").style.color = "red"
+    }
+
+    if (hasNumber(this.password)){
+      document.getElementById("number").style.color = "green"
+      this.strength++
+    }
+    else{
+      document.getElementById("number").style.color = "red"
+    }
+
+    if (this.password.length > 8){
+      document.getElementById("len").style.color = "green"
+      this.strength++
+    }
+    else{
+      document.getElementById("len").style.color = "red"
+    }
+
+  
+    document.getElementById("password-strength-meter").value = this.strength;
+  }
+
 }
 
+function hasUpper(str) {
 
-document.getElementById('inputBar').getElementById('passInput').oninput = function() {updateRequirements()};
+  for (let i = 0; i < str.length; i++) {
+    if (!isNaN(str[i] * 1)) {
+      continue
+    }
+    else {
+      if (str[i] == str[i].toUpperCase()){
+        return true
+      }
+    }
+  }
 
-function updateRequirements(){
-  //if this.password
-  alert("The value of the input field was changed.");
-  //document.getElementById("caps").innerText.fontcolor = "green"
-  //this.strength = this.strength + 1
+  return false
+}
 
-  //document.getElementById("password-strength-meter").value = "2";
+function hasLower(str) {
+
+  for (let i = 0; i < str.length; i++) {
+    if (!isNaN(str[i] * 1)) {
+      continue
+    }
+    else {
+      if (str[i] == str[i].toLowerCase()){
+        return true
+      }
+    }
+  }
+
+  return false
+}
+
+function hasNumber(str) {
+
+  for (let i = 0; i < str.length; i++) {
+    if (!isNaN(str[i] * 1)) {
+      return true
+    }
+  }
+
+  return false
 }
