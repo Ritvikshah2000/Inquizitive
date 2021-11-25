9402816b7f636321b50e24035659b08daeffda02
Akash Bhatthal <akash_bhatthal@sfu.ca>
2021-11-24 17:39:12 -0800

    Add database docs

--- inquizitive/quizzes/urls.py
+++ inquizitive/quizzes/urls.py
@@ -10,5 +10,4 @@ urlpatterns = [
     path('view_quiz_question/<int:pk>', views.QuizQuestionDetailView.as_view(), name='view_quiz_question'),
     path('<int:quiz_id>/create_quiz_qestion', views.QuizQuestionCreateView.as_view(), name='create_question'),
     path('<int:question_id>/add_question_option', views.QuizQuestionOptionCreateView.as_view(), name='add_question_option'),
-
 ]
