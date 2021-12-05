9bdec3f684d46e4303fdb500c7c90008b3a9ddbe
ritviks <ritvik_shah@sfu.ca>
2021-12-04 16:08:33 -0800

    added user profile, added ability for docker to handle media

--- inquizitive/inquizitive/settings.py
+++ inquizitive/inquizitive/settings.py
@@ -129,6 +129,9 @@ USE_TZ = True
 STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
+MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_URL = "/mediafiles/"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
